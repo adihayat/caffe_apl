f5e71d87c6ee7187cd63f5408d949a7e7fe25c2d
adih <adi.hayat3@gmail.com>
2016-11-19 21:04:34 +0200

    Resolving compilation errors

--- src/caffe/layers/apl_layer.cpp
+++ src/caffe/layers/apl_layer.cpp
@@ -6,7 +6,7 @@
 #include "caffe/common.hpp"
 #include "caffe/filler.hpp"
 #include "caffe/layer.hpp"
-#include "caffe/vision_layers.hpp"
+#include "caffe/neuron_layers.hpp"
 #include "caffe/util/math_functions.hpp"
 #include "caffe/syncedmem.hpp"
 #include <ctime>
